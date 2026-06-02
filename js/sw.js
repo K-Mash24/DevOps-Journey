4acd870f3d81da473134f9cfdf5352f0e7bdc71b
Keith Macharia <sc20130072023@student.mut.ac.ke>
2026-06-02 11:28:28 +0300

    New html Folder

--- js/sw.js
+++ js/sw.js
@@ -1,15 +1,19 @@
+// Makes your site work offline by storing a local copy of all important files in the user's browser cache. When the user visits your site again, the service worker intercepts the network requests and serves the cached files, allowing the site to load even without an internet connection. This is especially useful for improving performance and providing a seamless user experience, as it reduces load times and ensures that users can access your content regardless of their connectivity status.
+
 const CACHE_NAME = 'devops-journey-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/networking.html',
+  '/404.html',
   '/style.css',
   '/js/global.js',
   '/js/networking.js',
   '/img/favicon.svg',
   '/img/Network.svg',
   '/img/T568A vs T568B Practical Wiring Diagram.webp',
-  '/img/t568b-wiring-diagram-patch-panel-8086.jpg'
+  '/img/t568b-wiring-diagram-patch-panel-8086.jpg',
+  '/sitemap.xml',
 ];
 
 self.addEventListener('install', event => {
