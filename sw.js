27642470435dce87d4ddf3c825403712e33492ea
Keith Macharia <sc20130072023@student.mut.ac.ke>
2026-06-30 15:41:12 +0300

    Mobile repsonsiveness for the modal

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Format: YYYY-MM-DD-sequential (e.g., 2026-06-11-v1, 2026-06-11-v2, 2026-06-12-v1)
 // Starry sky completed
 
-const CACHE_NAME = 'devops-journey-2026-06-30-v4';
+const CACHE_NAME = 'devops-journey-2026-06-30-v5';
 
 const urlsToCache = [
   '/DevOps-Journey/',
